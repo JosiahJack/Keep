d8466ef0c162f03df59a0d6f9705dfe366c169b5
JosiahJack <josiahjackcitadel@gmail.com>
2022-07-08 07:11:05 -0500

    Huge cleanup effort to remove duplicate code found by cpd tool

---
+++ CopyPasteDetectorRun.sh
@@ -0,0 +1,3 @@
+% Run the pmd subroutine called cpd to detect duplicate code so I can reduce potential for duplicate bugs.
+/home/qmaster/Downloads/pmd/pmd-bin-6.47.0/bin/run.sh cpd --files /home/qmaster/QUAKE/keep/keepsrc/*.qc --minimum-tokens 100 --language c
+
